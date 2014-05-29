fd4b2c750c462a94574378f03878ca45cb03b603
yu <hubeiyichangzy@hotmail.com>
2014-05-29 13:18:51 +0800

    self-host app and client test

---
+++ client-app/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using self_host;
+
+namespace client_app
+{
+    internal class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private static void Main(string[] args)
+        {
+            client.BaseAddress = new Uri("http://localhost:5617");
+
+            ListAllProducts();
+            ListProduct(1);
+            ListProducts("toys");
+
+            Console.WriteLine("Press Enter to quit.");
+            Console.ReadLine();
+        }
+
+        private static void ListAllProducts()
+        {
+            HttpResponseMessage resp = client.GetAsync("api/products").Result;
+            resp.EnsureSuccessStatusCode();
+
+            var products = resp.Content.ReadAsAsync<IEnumerable<Product>>().Result;
+            foreach (var p in products)
+            {
+                Console.WriteLine("{0} {1} {2} ({3})", p.Id, p.Name, p.Price, p.Category);
+            }
+        }
+
+        private static void ListProduct(int id)
+        {
+            var resp = client.GetAsync(string.Format("api/products/{0}", id)).Result;
+            resp.EnsureSuccessStatusCode();
+
+            var product = resp.Content.ReadAsAsync<Product>().Result;
+            Console.WriteLine("ID {0}: {1}", id, product.Name);
+        }
+
+        private static void ListProducts(string category)
+        {
+            Console.WriteLine("Products in '{0}':", category);
+
+            string query = string.Format("api/products?category={0}", category);
+
+            var resp = client.GetAsync(query).Result;
+            resp.EnsureSuccessStatusCode();
+
+            var products = resp.Content.ReadAsAsync<IEnumerable<Product>>().Result;
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Name);
+            }
+        }
+    }
+}
---
+++ self-host/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.SelfHost;  
+
+namespace self_host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:5617");
+            
+            config.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
